aac14734e244c9a0d832e051409c4f4719b6f6ad
Luca Heiler <luca.h41@gso.schule.koeln>
2022-03-31 12:46:28 +0200

    Methoden SAS

---
+++ Metohten aufgabe 1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Methoden_aufgabe_1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("geben sie ein text ein");
+            string eingabe_string = Console.ReadLine();
+            int anzahl = 0;
+            bool flag;
+
+
+
+            flag = false;
+            Console.WriteLine("geben sie eine Zahl ein");
+            try
+            {
+
+
+
+
+                anzahl = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Fehler");
+                Console.Clear();
+                flag = true;
+
+            }
+
+            while (flag) ;
+
+            anzahl = ausgabeTextMalX(eingabe_string, anzahl);
+
+
+            Console.Write("\nAnzahlder Buchstaben: {0}", anzahl);
+            static int ausgabeTextMalX(string text, int anzahl1)
+
+
+                int b = 0;
+                int a = 0;
+
+            b = text.Length;
+            a = anzahl1;
+
+            a *= b;
+            Console.WriteLine(a);
+            for (int i = 0; i < anzahl1; i++)
+
+
+
+                Console.WriteLine(text);
+
+
+            anzahl1 = a;
+            Console.WriteLine(anzahl1);
+            return anzahl1;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
